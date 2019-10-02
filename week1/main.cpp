5d18dacc71223d9334f3781811ebe17408511a83
Forhad Ahmed <40792843+Ffforhad@users.noreply.github.com>
2019-10-02 16:27:46 +0600

    code upload

---
+++ week1/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   printf("robert frost said");
+   printf("\t two roads diverged in a yellow wood");
+   printf("\t\t and sorry i could not travel both");
+  return 0;
+}
